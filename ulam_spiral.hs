1efd109abe703d5cb0929b5563d2d5af16caa288
IMAYousaf <IMAYousaf@gmail.com>
2016-12-30 03:20:43 -0800

    Initial Commit
    Experiments with an Ulam Spiral and Haskell courtesy of !danb

---
+++ ulam_spiral.hs
@@ -0,0 +1,7 @@
+corners n = [base + c*oddN | c <- [0..3], let oddN = (2*n) - 1, let base = oddN^2 + oddN + 1]
+
+factors 1 = []
+factors n = k : (factors $ div n k)
+    where k = head [x | x <- [2..], mod n x == 0]
+
+isPrime = (==1) . length . take 2 . factors
