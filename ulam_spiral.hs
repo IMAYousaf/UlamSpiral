c009889ace48994403811dd814d09e7c218020be
IMAYousaf <IMAYousaf@gmail.com>
2016-12-30 12:25:02 -0800

    Fixed Corners

--- ulam_spiral.hs
+++ ulam_spiral.hs
@@ -1,4 +1,4 @@
-corners n = [base + c*oddN | c <- [0..3], let oddN = (2*n) - 1, let base = oddN^2 + oddN + 1]
+corners n = [base + c*oddN | c <- [0..3], let oddN = (2*n) - 1, let base = oddN^2 + oddN + 1 + c]
 
 factors 1 = []
 factors n = k : (factors $ div n k)
